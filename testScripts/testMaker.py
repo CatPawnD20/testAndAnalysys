af893abe197b44636a851fd38746f56f6f4cd930
CatPawnD20 <ata.osman.ozguz@gmail.com>
2024-10-03 14:52:10 +0300

    monthly before ready

--- testScripts/testMaker.py
+++ testScripts/testMaker.py
@@ -1,20 +1,56 @@
 import datetime
 
+import config
 from testScripts import testDataPrepatory, tradeMaker
 
 
+
+def calculate_reset_money(testData):
+    reset_money_trade_dict = {}
+    for trade in testData:
+        date = trade[1]
+        month = date.month
+        if month in reset_money_trade_dict:
+            # Mevcut tarihle karşılaştırıp daha erken olanı seçiyoruz
+            if date < reset_money_trade_dict[month]:
+                reset_money_trade_dict[month] = date
+        else:
+            reset_money_trade_dict[month] = date
+    return reset_money_trade_dict
+
+
+
 def generateTradeList(testDataList):
-    #do one trade for each testData from testDataList
-    tradeList = []
-    for testData in testDataList:
-        trade = tradeMaker.makeTrade(testData)
-        tradeList.append(trade)
-        if tradeMaker.before_cash_global == 0 and tradeMaker.before_bitcoin_global == 0:
-            #stop the test if the cash and bitcoin are 0
-            break
-    tradeMaker.before_cash_global = 0
-    tradeMaker.before_bitcoin_global = 0
-    return tradeList
+    global reset_for_month
+    if config.monthly_test:
+        tradeList = []
+        reset_money_trade_dict = calculate_reset_money(testDataList)
+        for testData in testDataList:
+            if testData[1] in reset_money_trade_dict.values():
+                reset_for_month = True
+            trade = tradeMaker.makeTrade(testData)
+            reset_for_month = False
+            tradeList.append(trade)
+            if tradeMaker.before_cash_global == 0 and tradeMaker.before_bitcoin_global == 0:
+                # stop the test if the cash and bitcoin are 0
+                break
+        tradeMaker.before_cash_global = 0
+        tradeMaker.before_bitcoin_global = 0
+        return tradeList
+    else:
+        tradeList = []
+        for testData in testDataList:
+            trade = tradeMaker.makeTrade(testData)
+            tradeList.append(trade)
+            if tradeMaker.before_cash_global == 0 and tradeMaker.before_bitcoin_global == 0:
+                # stop the test if the cash and bitcoin are 0
+                break
+        tradeMaker.before_cash_global = 0
+        tradeMaker.before_bitcoin_global = 0
+        return tradeList
+
+def get_reset_for_month():
+    return reset_for_month
 
 
 def doTest():
@@ -30,3 +66,4 @@ def doTest():
     tradeMaker.printTradeList(controlledTradeList)
 
     return controlledTradeList
+
